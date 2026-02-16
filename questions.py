6d460b0e82dc680885841e5777d49b58e8a70ed9
creator154 <cteator898@gmail.com>
2026-02-16 17:12:59 +0530

    Create questions.py

---
+++ questions.py
@@ -0,0 +1,12 @@
+quiz_data = [
+    {
+        "question": "2 + 2 = ?",
+        "options": ["3", "4", "5", "6"],
+        "correct": 1
+    },
+    {
+        "question": "Capital of India?",
+        "options": ["Mumbai", "Delhi", "Kolkata", "Chennai"],
+        "correct": 1
+    },
+]
